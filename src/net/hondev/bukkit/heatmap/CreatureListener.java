52334f54861ce41f02bfc810da18194ef3ade88c
Sikerdebaard <sikerdebaard@gmail.com>
2011-05-24 15:15:28 +0200

    Added propper spawn isCanceled check.
    Tamed wolfs will no longer be added to the spawn log.
    
    CreatureListener and WorldSaveListener are now registered as Monitor
    mode instead of priority low.
    
    Change-Id: I35d0da64a533779a706609f6a081c74d7a0a0def
    TODO: offload buffer save to ASyncTask;

--- src/net/hondev/bukkit/heatmap/CreatureListener.java
+++ src/net/hondev/bukkit/heatmap/CreatureListener.java
@@ -6,10 +6,6 @@
 
 package net.hondev.bukkit.heatmap;
 
-import java.util.concurrent.DelayQueue;
-import java.util.concurrent.Delayed;
-import java.util.concurrent.TimeUnit;
-
 import org.bukkit.Chunk;
 import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Wolf;
@@ -20,70 +16,20 @@ import org.bukkit.event.entity.EntityListener;
 public class CreatureListener extends EntityListener  {
 	public static HeatMap plugin;
 	
-	private DelayQueue<Spawn> queue;
-	
 	public CreatureListener(HeatMap instance){
 		super();
-		queue = new DelayQueue<Spawn>();
 		CreatureListener.plugin = instance;
 	}
 	
 	@Override
 	public void onCreatureSpawn(CreatureSpawnEvent event){
+		if(event.isCancelled()) // ignore canceled spawns
+			return;
+		
 		if(event.getCreatureType() == CreatureType.WOLF && event.getEntity() instanceof Wolf && ((Wolf) event.getEntity()).isTamed()) // ignore tamed wolfs due to respawn (bug?)
 			return;
 		
-		queue.add(new Spawn(event));
-		processQueue();
-	}
-	
-	private void processQueue(){
-		Spawn spawn;
-		CreatureSpawnEvent event;
-		Chunk chunk;
-		while((spawn = queue.poll()) != null){
-			event = spawn.event;
-			
-			if(!event.isCancelled()){
-				chunk = event.getLocation().getBlock().getChunk();
-				plugin.registerSpawn(chunk.getX(), chunk.getZ(), chunk.getWorld().getId(), event.getCreatureType().ordinal(), spawn.time);
-			}
-			else {
-				plugin.getServer().getLogger().info("Spawn event canceled.");
-			}
-		}
-	}
-	
-	private class Spawn implements Delayed {
-		protected CreatureSpawnEvent event;
-		protected long time;
-		
-		private long endOfDelay;
-		
-		public Spawn(CreatureSpawnEvent event){
-			this.event = event;
-			time = System.currentTimeMillis();
-			endOfDelay = System.currentTimeMillis() + 10000;
-		}
-		@Override
-		public int compareTo(Delayed delay) {
-			if(!(delay instanceof Spawn))
-				return -1;
-			
-			Spawn s = (Spawn) delay;
-			
-			if(this.endOfDelay < s.endOfDelay)
-				return -1;
-			else if(this.endOfDelay > s.endOfDelay)
-				return 1;
-			
-			
-			return 0;
-		}
-		@Override
-		public long getDelay(TimeUnit timeUnit) {
-			return timeUnit.convert(endOfDelay - System.currentTimeMillis(),
-                    TimeUnit.MILLISECONDS);
-		}
+		Chunk chunk = event.getLocation().getBlock().getChunk();
+		plugin.registerSpawn(chunk.getX(), chunk.getZ(), chunk.getWorld().getId(), event.getCreatureType().ordinal(), System.currentTimeMillis());
 	}
 }
